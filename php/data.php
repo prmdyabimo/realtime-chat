098eb96cfafec21a321d92db0ab81acc85d27415
prmdyabimo <pramudyabimo1920@gmail.com>
2022-08-09 21:51:55 +0700

    commit

--- php/data.php
+++ php/data.php
@@ -2,7 +2,7 @@
 
     while ($row = mysqli_fetch_assoc($sql)) {
       $output .= '
-          <a href="" class="flex items-center justify-between pb-[20px] pr-[15px] border-b-2 border-slate-100">
+          <a href="chat.php?user_id='. $row['unique_id'] .'" class="flex items-center justify-between pb-[20px] pr-[15px] border-b-2 border-slate-100">
             <div class="flex mt-4 mb-0 items-center ">
               <img src="php/image_users/'. $row['img'] .'" alt="" class="w-[40px] h-[40px] object-cover rounded-full">
               <div class="ml-[15px]">
